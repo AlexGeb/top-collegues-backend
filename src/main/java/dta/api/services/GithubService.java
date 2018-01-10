97e503f2a8cff6cd3ac13563a700b56a16425ae6
AlexGeb <alex.behaghel@gmail.com>
2018-01-10 18:10:55 +0100

    hide secret

--- src/main/java/dta/api/services/GithubService.java
+++ src/main/java/dta/api/services/GithubService.java
@@ -6,6 +6,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GithubService {
-	  @GET("users/{pseudo}?access_token=6b4d1059ad3605c3a1912e5f1cf98e1dfdf84d97")
+	  @GET("users/{pseudo}?access_token=github token")
 	  Call<GithubUser> getUserInfo(@Path("pseudo") String pseudo);
 }
