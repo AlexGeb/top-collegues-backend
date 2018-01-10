251ff9d7f1f615cbcc421a87bd90f7301eda0265
AlexGeb <alex.behaghel@gmail.com>
2018-01-10 17:35:20 +0100

    delete target and see what's happenning

--- src/main/java/dta/api/services/GithubService.java
+++ src/main/java/dta/api/services/GithubService.java
@@ -6,6 +6,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GithubService {
-	  @GET("users/{pseudo}?access_token=github_auth")
+	  @GET("users/{pseudo}?access_token=6b4d1059ad3605c3a1912e5f1cf98e1dfdf84d97")
 	  Call<GithubUser> getUserInfo(@Path("pseudo") String pseudo);
 }
